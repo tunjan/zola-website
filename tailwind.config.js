2a4b1e6f2863f996a54068f950cfffb7120dd912
Alberto Trigo Medina <albertomedinatrigo@gmail.com>
2024-12-01 16:00:34 +0100

    *

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
     fontFamily: {
-      'sans': ['Inter']
+      'sans': ['IBM Plex Sans'],
+      'serif': ['IBM Plex Serif']
+
+    },
+    colors: {
+      'forest': '#2f3c1d',
+      'bark': '#fdf8db',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+
     },
     extend: {},
   },
